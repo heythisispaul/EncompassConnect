47ade93f4bbd573cd97bc99bffc8c96efb7033dd
Paul Richard <heythisispaul@gmail.com>
2018-10-23 23:18:31 -0700

    added basic types and PipelineView Info

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,22 @@ export default class EncompassConnect {
         this.instanceId = instaceId;
     }
 
+    private callInfo = (method: string, token: string, body?: any): any => {
+        let options: any = {
+            method: method,
+            headers: {
+                "Authorization": 'Bearer ' + token
+            },
+            dataType: 'text',
+            contentType: 'application/x-www-form-urlencoded',
+            json: true
+        };
+        if (body) {
+            options.body = body;
+        }
+        return options;
+    }
+
     authenticate = (username: string, password: string): Promise<string | boolean> => {
         let auth = "Basic " + Buffer.from(this.clientId + ":" + this.APIsecret).toString('base64');
         let dataString = `grant_type=password&username=${username}@encompass:${this.instanceId}&password=${password}`;
@@ -33,4 +49,71 @@ export default class EncompassConnect {
             });
         });
     }
+
+    getGuid = (token: string, loanNumber: string): Promise<string | boolean> => {
+        let options = this.callInfo('POST', token, {
+            "filter": {
+                "operator": "and",
+                "terms": [{
+                  canonicalName: "Loan.LoanNumber",
+                  value: loanNumber,
+                  matchType: "exact"
+              }]
+            },
+            "sortOrder": [
+                {
+                    "canonicalName": "Loan.LastModified",
+                    "order": "desc"
+                }
+            ],
+            "fields": ["Loan.GUID"]
+        });
+
+        return new Promise((resolve, reject) => {
+            request('https://api.elliemae.com/encompass/v1/loanPipeline/', options, (err: any, response: any, body: any) => {
+                if (err) {
+                    reject(err);
+                }
+                if (response.body.length > 0) {
+                    resolve(response.body[0].loanGuid);
+                }
+                else {
+                    reject(`Loan ${loanNumber} did not return a matching GUID`);
+                }
+            });
+        });
+    }
+
+    pipeLineView = (token: string, options: PipeLineContract): Promise<any> => {
+        let requestOptions = this.callInfo('POST', token, options);
+        return new Promise((resolve, reject) => {
+            request('https://api.elliemae.com/encompass/v1/loanPipeline/', requestOptions, (err, response) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(response.body);
+            });
+        });
+    }
 }
+
+export interface PipeLineContract {
+    filter: {
+      operator: "and" | "or";
+      terms: PipeLineTerms[];
+    },
+    sortOrder?: sortOrderContract[];
+    fields: string[];
+  }
+  
+  export interface PipeLineTerms {
+    canonicalName: string;
+    matchType: "greaterThanOrEquals" | "exact" | "greaterThan" | "isNotEmpty" | "isEmpty" | "lessThan" | "lessThanOrEquals" | "equals" | "notEquals" | "startsWith" | "contains";
+    value?: string | number;
+    precision?: "exact"| "day" | "month" | "year" | "recurring";
+  }
+  
+  export interface sortOrderContract {
+    canonicalName: string;
+    order: "asc" | "desc";
+  }
