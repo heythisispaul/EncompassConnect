ab7b01e4e13e8ed21d4c7624a2bd201bd3e62518
Paul Richard <heythisispaul@gmail.com>
2021-01-22 09:30:47 -0700

    feat(request): remove encompass and version slug from generic request call

--- src/types.ts
+++ src/types.ts
@@ -125,6 +125,7 @@ export interface InternalRequestOptions {
   isRetry?: boolean;
   isNotJson?: boolean;
   version?: number;
+  useTruncatedBase?: boolean;
 }
 
 export interface AssignMilestoneOptions {
